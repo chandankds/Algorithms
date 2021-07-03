252e4c92623553baabfe7ae7b88282bb5a384816
chandankds <chandankds@gmail.com>
2021-07-03 21:38:18 +0530

    Add project files.

---
+++ Algorithms/Algorithms.cpp
@@ -0,0 +1,12 @@
+﻿// Algorithms.cpp : Defines the entry point for the application.
+//
+
+#include "Algorithms.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ Searching/main.cpp
@@ -0,0 +1,12 @@
+// Algorithms.cpp : Defines the entry point for the application.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello Hell" << endl;
+	return 0;
+}
